e44b9ad357639d6d5c424fd78aab628a3a6afc06
Luiz Fernando <lyiz.feh12rodrigues@gmail.com>
2021-03-10 17:32:43 -0300

    finalizando 3parte mvvm
    finalizando 3parte mvvm

--- app/src/main/java/com/example/mylist/MainActivity.kt
+++ app/src/main/java/com/example/mylist/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.mylist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mylist.model.Pais
 
 class MainActivity : AppCompatActivity() {
+
+    private val adapterPaises = PaisesAdapter()
+    private var list: RecyclerView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        list = findViewById(R.id.listPaises)
+        val paises: List<Pais> =
+            listOf(Pais("Brasil"), Pais("Alemanha"), Pais("Estados Unidos"), Pais("Russia"))
+        adapterPaises.items = paises
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        with(list) {
+            this?.adapter = adapterPaises
+            val manager = androidx.recyclerview.widget.LinearLayoutManager(this?.context)
+            list?.layoutManager = manager
+        }
     }
 }
---
+++ app/src/main/java/com/example/mylist/model/Pais.kt
@@ -0,0 +1,3 @@
+package com.example.mylist.model
+
+data class Pais (var name: String)
